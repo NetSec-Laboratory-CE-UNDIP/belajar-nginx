1bd2475f1b18682c226e50a5d3426e8eee2cd80e
luqmansen <luqmansen@gmail.com>
2020-11-26 00:17:53 +0700

    Update example app to support python3

--- lesson-2/server.py
+++ lesson-2/server.py
@@ -9,16 +9,13 @@ def createServer():
         clientsocket, address = serversocket.accept()
         message = clientsocket.recv(4096)
         path = message.split()[1]
-        if path == "/admin":
-            clientsocket.send("HTTP/1.1 200 OK\n"
-            +"Content-Type: text/html\n"
-            +"\n" 
-            +"<html><body>This is admin page, no one should be here</body></html>\n")
+        
+        if path == b"/admin":
+            msg = "HTTP/1.1 200 OK\n Content-Type: text/html \n\n <html><body>This is admin page, no one should be here</body></html>\n"
+            clientsocket.send(msg.encode())
         else:
-            clientsocket.send("HTTP/1.1 200 OK\n"
-            +"Content-Type: text/html\n"
-            +"\n" 
-            +"<html><body>Hello from server {}</body></html>\n".format(os.getenv('PORT')))
+            msg = f"HTTP/1.1 200 OK\n Content-Type: text/html \n\n <html><body>Hello from server ID={str(os.getenv('PORT'))} </body></html>\n"
+            clientsocket.send(msg.encode())
 
         clientsocket.shutdown(SHUT_WR)
         clientsocket.close()
